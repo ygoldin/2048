07b90a99ec39fa6e372ea72e8c911aa24e3d033b
Yael Goldin <ygoldin@cs.washington.edu>
2018-08-04 16:21:43 -0700

    numbers show up on grid

--- src/Frame_2048.java
+++ src/Frame_2048.java
@@ -1,12 +1,15 @@
 import java.awt.*;
 import javax.swing.*;
 
+@SuppressWarnings("serial")
 public class Frame_2048 extends JFrame {
 	
 	private Grid grid2048;
 	private GridSpot[][] tiles;
 
 	public Frame_2048() {
+		grid2048 = new Grid();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setMinimumSize(new Dimension(1024, 768));
 		setTitle("2048");
@@ -23,14 +26,30 @@ public class Frame_2048 extends JFrame {
 		add(mainGridTilePanel);
 	}
 	
-	//this class represents one of the buttons on the tic tac toe grid
+	//this class represents one of the buttons on the 2048 grid
 	private class GridSpot extends JButton {
 		private static final int FONT_SIZE = 80;
 		private static final String FONT_NAME = "Arial";
+		private int row;
+		private int col;
 		
 		public GridSpot(int row, int col) {
+			super();
+			setOpaque(true);
+			setRolloverEnabled(false);
 			setFont(new Font(FONT_NAME, Font.PLAIN, FONT_SIZE));
-			
+			this.row = row;
+			this.col = col;
+		}
+		
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			int value = grid2048.valueAtLocation(row, col);
+			if(value != 0) {
+				setText("" + value);
+			} else {
+				setText(null);
+			}
 		}
 	}
 }
